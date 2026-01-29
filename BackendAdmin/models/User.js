1815ec2e86ea4028a6cbf408d988fafd5dbfe570
jegathesh7 <pjegathesh@gmail.com>
2026-01-29 21:33:14 +0530

    5th commit-JWT without RBC

--- BackendAdmin/models/User.js
+++ BackendAdmin/models/User.js
@@ -1,15 +1,13 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    collegeName: { type: String, required: true },
-    batch: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    collegeName: { type: String, required: true },email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     status: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
 
-export default mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
