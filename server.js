ea68d7692a0a25b0a8498252eec85b88e01318ca
Poovarasan S <poovarasan936161@gmail.com>
2026-01-30 23:48:53 +0530

    fix:server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const dotenv = require("dotenv");
-dotenv.config();  
+dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db.js");
@@ -7,21 +7,22 @@ const connectDB = require("./config/db.js");
 const app = express();
 connectDB();
 
+const allowedOrigins = process.env.CLIENT_URL
+  ? process.env.CLIENT_URL.split(",").map((url) => url.trim())
+  : [];
 
-app.use(cors({
-  origin: "https://localhost:5173", // Frontend URL
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require("cookie-parser")());
 
-
 // Make uploads folder static
 app.use("/uploads", express.static("uploads"));
-
-
-
 
 // ✅ ROUTES (paths must be EXACT)
 app.use("/api/events", require("./routes/eventRoutes"));
@@ -29,7 +30,6 @@ app.use("/api/achievers", require("./routes/achieverRoutes"));
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/unstop", require("./routes/proxyRoutes"));
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
