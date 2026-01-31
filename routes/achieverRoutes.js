3b59fa7deecffbba6690e13315c6d148f45aace1
Poovarasan S <poovarasan936161@gmail.com>
2026-01-31 18:50:29 +0530

    feat: added api to update reaction

--- routes/achieverRoutes.js
+++ routes/achieverRoutes.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { createAchiever, getAchievers, updateAchiever,deleteAchiever } = require("../controllers/achieverController");
-
+const {
+  createAchiever,
+  getAchievers,
+  updateAchiever,
+  deleteAchiever,
+  updateReaction,
+} = require("../controllers/achieverController");
 
 const upload = require("../middleware/upload");
 const { protect, adminOnly } = require("../middleware/authMiddleware");
 
-router.post("/",protect,adminOnly, upload.any(), createAchiever);
-router.get("/", protect,getAchievers);
-router.put("/:id", protect,adminOnly, upload.any(), updateAchiever);
-router.delete("/:id", protect,adminOnly, deleteAchiever);
-
+router.post("/", protect, adminOnly, upload.any(), createAchiever);
+router.get("/", protect, getAchievers);
+router.put("/:id", protect, adminOnly, upload.any(), updateAchiever);
+router.delete("/:id", protect, adminOnly, deleteAchiever);
+router.patch("/reaction/:id", protect, updateReaction);
 
 module.exports = router;
