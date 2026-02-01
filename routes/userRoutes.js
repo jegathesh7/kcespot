84fb4302d7e8cdbda953cc73b1221188050efe76
Poovarasan S <poovarasan936161@gmail.com>
2026-02-02 01:15:28 +0530

    feat: push notifications

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ const {
   updateUser,
   deleteUser,
 } = require("../controllers/userController");
-
+const { savePushToken } = require("../controllers/notificationController");
 const { protect, adminOnly } = require("../middleware/authMiddleware");
 
 // 🔐 ADMIN ACCESS ONLY
@@ -15,5 +15,6 @@ router.get("/", protect, adminOnly, getUsers);
 router.post("/", protect, adminOnly, createUser);
 router.put("/:id", protect, adminOnly, updateUser);
 router.delete("/:id", protect, adminOnly, deleteUser);
+router.post("/save-push-token", protect, savePushToken);
 
 module.exports = router;
