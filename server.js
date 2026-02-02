06ef52c136995273288fb016f2dfa404fe409ce9
Poovarasan S <poovarasan936161@gmail.com>
2026-02-02 18:00:39 +0530

    fix:ngrok skip browser

--- server.js
+++ server.js
@@ -32,7 +32,12 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-App-Client"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-App-Client",
+    "ngrok-skip-browser-warning",
+  ],
 };
 
 app.use(cors(corsOptions));
