204bb29c901d22a983576699c50cbc1ef0eeb8c8
Poovarasan S <poovarasan936161@gmail.com>
2026-02-03 15:18:34 +0530

    fix:patch

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -20,7 +20,7 @@ router.post("/get-me", protect, getUser);
 // 🔐 ADMIN ACCESS ONLY
 router.get("/", protect, adminOnly, getUsers);
 router.post("/", protect, adminOnly, createUser);
-router.put("/:id", protect, updateUser);
+router.patch("/:id", protect, updateUser);
 router.delete("/:id", protect, adminOnly, deleteUser);
 router.post("/save-push-token", protect, savePushToken);
 router.post("/save-token", protect, savePushToken); // Alias for compatibility
