48c03b1b4adc2f5abf36803f375506d5889fad17
Poovarasan S <poovarasan936161@gmail.com>
2026-02-13 13:46:52 +0530

    fix: patch in methods in server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const corsOptions = {
     return callback(new Error(`CORS blocked: ${origin}`));
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], 
   allowedHeaders: [
     "Content-Type",
     "Authorization",
